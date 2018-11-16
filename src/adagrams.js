23ff1e610d044e68d93343bf1a791b4181b3e529
jackie <jacquelyn.cheng@me.com>
2018-11-15 19:58:44 -0800

    Minor formatting changes around line breaks

--- src/adagrams.js
+++ src/adagrams.js
@@ -68,9 +68,11 @@ const Adagrams = {
   scoreWord(word) {
     let score = 0;
 
-    const points = { 'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
-    'D': 2, 'G': 2, 'B': 3, 'C': 3, 'M': 3, 'P': 3, 'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
-    'K': 5, 'J': 8, 'X': 8, 'Q': 10, 'Z': 10}
+    const points = {
+      'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+      'D': 2, 'G': 2, 'B': 3, 'C': 3, 'M': 3, 'P': 3, 'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+      'K': 5, 'J': 8, 'X': 8, 'Q': 10, 'Z': 10
+    }
 
     word.toUpperCase().split('').forEach( (letter) => score += points[letter] );
 
@@ -103,7 +105,6 @@ const Adagrams = {
 
     return highest;
   }
-
 };
 
 // Do not remove this line or your tests will break!
